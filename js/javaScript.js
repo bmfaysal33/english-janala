e1da24e4eccc721b15e5923058045f9ebc45e5b5
BM Faysal <bmfaysal33@gmail.com>
2025-10-19 10:57:02 +0600

    fixed not showing words or not calling API, now showing and okay

--- js/javaScript.js
+++ js/javaScript.js
@@ -6,29 +6,49 @@ const loadLessons = () => {
         .then(json => displayLessons(json.data));
 }
 
+
+
+
 const loadLevelWord = (id) => {
 
-    const url = `https://openapi.programming-hero.com/api/level/${id}`
+    let url = `https://openapi.programming-hero.com/api/level/${id}`
     // console.log(url)
     fetch(url)
-        .then(res => res.json())
-        .then(data => console.log(data.data))
+        .then((res) => res.json())
+        .then((data) => displayWordsByLevel(data.data))
 
 }
 
+
 const displayWordsByLevel = (words) => {
+
     const wordContainer = document.getElementById("word-container");
     wordContainer.innerHTML = '';
 
-    for (let word of words ){
-        
+    words.forEach(word => {
+
         console.log(word);
 
-        // const card = document.createElement('div');
-        // card.innerHTML = ` <p> Cat </p> `
-        // wordContainer.append(card)
-    }
-   
+        const card = document.createElement('div');
+        card.innerHTML = `  <div class="bg-white min-h-40 m-4 p-10 space-y-4 text-center rounded-2xl">
+        <h1 class="font-bold text-3xl">${word.word}</h1>
+        <p class="font-semiboldl">Meaning /Pronounciation</p>
+        <p class="font-medium font-bangla text-2xl">"${word.meaning} / ${word.pronunciation}"</p>
+
+        <div class="flex justify-between items-center">
+          <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF90]">
+            <i class="fa-solid text-2xl fa-circle-info"></i>
+          </button>
+          <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF90]">
+            <i class="fa-solid text-2xl fa-volume-high"></i>
+          </button>
+        </div>
+
+      </div> `
+        wordContainer.append(card)
+
+    });
+
 
 };
 
@@ -39,17 +59,16 @@ const displayLessons = (lessons) => {
     lessonConatiner.innerHTML = '';
 
     //2. get into every lessons
+
     for (let lesson of lessons) {
+
         // create element
-
-
         const btnDiv = document.createElement('div');
         btnDiv.innerHTML = ` <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary">
             <i class="fa-solid fa-book-open-reader"></i> Lesson - ${lesson.level_no}
-          </button>
-            `
-        // append into container
+          </button> `
 
+        // append into container
         lessonConatiner.appendChild(btnDiv)
 
     }
